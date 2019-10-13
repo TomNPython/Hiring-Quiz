55d834c744ee29d5c0d1e6f764e445358f774622
tomni <tom.nicholson1000@gmail.com>
2019-10-13 15:47:17 +0700

    started refactoring js

--- main.js
+++ main.js
@@ -8,64 +8,61 @@ const answerTwo = document.getElementById('answer-two')
 const answerThree = document.getElementById('answer-three')
 const answerFour = document.getElementById('answer-four')
 
-const answerSheet = document.getElementById('answersheet')
+const answerSheet = document.getElementById('answersheet') 
 let answers = 0
 const score = document.getElementById('score')
 const message = document.getElementById('message')
 
 function oneAnswer() {
-    questionOneContainer.style.opacity = '0'
-    questionOneContainer.style.height = '0'
-    questionOneContainer.style.visibility = 'hidden'
+    questionOneContainer.style.display = 'none'
     questionTwoContainer.style.opacity = '1'
 }
 
 function twoAnswer() {
-    questionTwoContainer.style.opacity = '0'
-    questionTwoContainer.style.height = '0'
-    questionTwoContainer.style.visibility = 'hidden'
+    questionTwoContainer.style.display = 'none'
     questionThreeContainer.style.opacity = '1'
 }
 
 function threeAnswer() {
-    questionThreeContainer.style.opacity = '0'
-    questionThreeContainer.style.height = '0'
-    questionThreeContainer.style.visibility = 'hidden'
+    questionThreeContainer.style.display = 'none'
     questionFourContainer.style.opacity = '1'
 }
 
 function fourAnswer() {
-    questionFourContainer.style.opacity = '0'
-    questionFourContainer.style.height = '0'
-    questionFourContainer.style.visibility = 'hidden'
+    questionFourContainer.style.display = 'none'
     answersheet.style.opacity = '1'
 }
-
 
 questionOneContainer.addEventListener('click', oneAnswer, true)
 questionTwoContainer.addEventListener('click', twoAnswer, true)
 questionThreeContainer.addEventListener('click', threeAnswer, true)
-questionFourContainer.addEventListener('click', fourAnswer, true)
+questionFourContainer.addEventListener('click', fourAnswer, true) 
 
 function correctAnswer() {
     answers++
     score.innerHTML = answers + ' / 4'
-    if (answers === 0) {
-        message.innerHTML = "Sorry, try again."
-    } else if (answers === 1) {
-        message.innerHTML = "It's a good start, but you can do better."
-    } else if (answers === 2) {
-        message.innerHTML = "Nearly there. Nice try!"
-    } else if (answers === 3) {
-        message.innerHTML = "So close."
-    } else {
-        message.innerHTML = "Congratulations. You've made some fine choices!"
+    
+    switch(answers) {
+        case 0:
+            message.innerHTML = "Sorry, try again."
+            break;
+        case 1:
+            message.innerHTML = "It's a good start, but you can do better."
+            break;
+        case 2: 
+            message.innerHTML = "Nearly there. Nice try!"
+            break;
+        case 3:
+            message.innerHTML = "So close."
+            break;
+        case 4:
+            message.innerHTML = "Congratulations! You've made some fine choices!"
     }
 }
 
-answerOne.addEventListener('click', correctAnswer)
-answerTwo.addEventListener('click', correctAnswer)
-answerThree.addEventListener('click', correctAnswer)
-answerFour.addEventListener('click', correctAnswer)
+const questions = document.querySelectorAll('.answer')
+
+questions.forEach(question => question.addEventListener('click', correctAnswer))
+
 
 
